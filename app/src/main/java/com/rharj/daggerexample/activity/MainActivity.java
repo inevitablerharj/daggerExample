47a57eb341c9f5dbd3390f33719872340ff94f77
Raji Oladayo <oladayo.raji@techadvance.ng>
2018-01-14 10:06:13 +0100

    Add project dependencies

--- app/src/main/java/com/rharj/daggerexample/activity/MainActivity.java
+++ app/src/main/java/com/rharj/daggerexample/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.rharj.daggerexample;
+package com.rharj.daggerexample.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.rharj.daggerexample.R;
 
 public class MainActivity extends AppCompatActivity {
 
